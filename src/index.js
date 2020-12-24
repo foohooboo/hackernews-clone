8ff631bfc94e5db883e38546e11888f7ed4d5b7c
foohooboo <danielmarshallevans@gmail.com>
2020-12-24 14:38:24 -0700

    switched resolvers to prisma and new sqlite db

--- src/index.js
+++ src/index.js
@@ -1,56 +1,34 @@
+const { PrismaClient } = require("@prisma/client")
 const { ApolloServer } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
+const { info } = require('console');
 
-let dummyLinks = [
-    {
-        id: 'link-0',
-        url: 'www.google.com',
-        description: 'It looks stuff up good.'
-    },
-    {
-        id: 'link-1',
-        url: 'www.facebook.com',
-        description: 'The bane and benifit of modern thought sharing.'
-    },
-]
 
-let idCount = dummyLinks.length
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
-        link: (parent, args) => dummyLinks.find(link => link.id === args.id),
-        feed: () => dummyLinks,
-        count: () => dummyLinks.length,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
+        link: async (parent, args, context) => {
+            context.prisma.link.find()
+        }
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            dummyLinks.push(link)
-            return dummyLinks[dummyLinks.length - 1]
-        },
-        updateLink: (parent, args) => {
-            let index = dummyLinks.findIndex(link => link.id === args.id)
-            if(index > -1){
-                args.url ? dummyLinks[index].url = args.url : null
-                args.description ? dummyLinks[index].description = args.description : null
-            }
-            return dummyLinks[index]
-        },
-        deleteLink: (parent, args) => {
-            let index = dummyLinks.findIndex(link => link.id === args.id)
-            let link = dummyLinks[index]
-            if(link) {
-                dummyLinks.splice(index, 1)
-            }
-            return link
+        post: (parent, args, context, info) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            })
+            return newLink
         },
     },
 }
+
+const prisma = new PrismaClient()
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
@@ -58,10 +36,13 @@ const server = new ApolloServer({
         'utf8'
     ),
     resolvers,
+    context: {
+        prisma,
+    }
 })
 
 server
     .listen()
     .then(({ url }) => 
-        console.log('Server is running on {url}')
+        console.log(`Server is running on ${url}`)
     );
